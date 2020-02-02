5197baa4cd99c6d3341729ce9be44087b1eb9361
pmasa <58562027+pmasa@users.noreply.github.com>
2020-02-02 21:06:02 +0100

    Update niveau-eau-pushbullet.py

--- niveau-eau-pushbullet.py
+++ niveau-eau-pushbullet.py
@@ -69,7 +69,7 @@ def send_notification_via_pushbullet(title, body):
     """
     data_send = {"type": "note", "title": title, "body": body}
  
-    ACCESS_TOKEN = 'o.Tc4lPsyu1U1vyRTwACB58zgJ2xFynMpC'
+    ACCESS_TOKEN = 'xxxxx'
     resp = requests.post('https://api.pushbullet.com/v2/pushes', data=json.dumps(data_send),
                          headers={'Authorization': 'Bearer ' + ACCESS_TOKEN, 'Content-Type': 'application/json'})
     if resp.status_code != 200:
@@ -93,7 +93,7 @@ while True:
 	#myriam = pb.contact[0]
 	#push = myriam.push_note("Alert !!", "Niveau d'innondation atteint")	
 	print(gsm2)
-	push = pb.push_sms(gsm2, "+32476783818", "Alert innondation !!!")
+	push = pb.push_sms(gsm2, "+32477123456", "Alert innondation !!!")
         #push = gsm2.push_note("Alert GSM2!!", "Niveau d'innondation atteint")
         #push = pb.push_note("Hello world!", "We're using the api.", contact="myriam masamuna")
 	send_notification_via_pushbullet("Alert innondation!!", "Niveau d'innodation atteint")
